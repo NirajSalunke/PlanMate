3eaae5e939c71a7c60e1246b92cb6656a861e4c6
Kaustubh Gharat <gharatkaustubh36@gmail.com>
2024-10-03 19:34:54 +0530

    calendar added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import Calendar from "react-calendar";
+import CalendarComponent from "../components/CalendarComponent";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: HomeComponent,
-})
+});
 
 function HomeComponent() {
   return (
-    <div className="p-2">
+    <div className="p-2" >
       <h3>Welcome Home!</h3>
+      <CalendarComponent />
     </div>
-  )
+  );
 }
